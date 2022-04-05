9ca73232d24b2c984185ee97e5658ee3d4f4bfea
João Neto <netojoao599@gmail.com>
2022-04-05 12:24:49 -0300

    demo primeiro front

---
+++ js/acoes.js
@@ -0,0 +1,20 @@
+function abrirMenu() {
+    document.getElementById('menu').style.width = '250px';
+    document.getElementById('conteudo').style.marginLeft = '1280px';
+   
+}
+
+function fecharMenu() {
+    document.getElementById('menu').style.width = '0px';
+    document.getElementById('conteudo').style.marginLeft = '0px';
+}
+
+
+
+/*
+ 
+*/
+
+
+
+
---
+++ readme.txt
@@ -0,0 +1,9 @@
+
+gerador RGB: https://cssgradient.io/
+
+gerador de icons: https://favicon.io/
+
+
+
+
+
